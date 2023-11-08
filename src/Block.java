cb72064e37bb9454b794d322b15ffa806ecc9a07
KFitz1123 <Kfitz53090@gmail.com>
2023-11-07 21:30:20 -0600

    various chng

--- src/Block.java
+++ src/Block.java
@@ -1,6 +1,7 @@
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchAlgorithmException;  
+
 
 public class Block {
     private int num;
@@ -8,14 +9,17 @@ public class Block {
     private long nonce;
     private Hash prevHash;
     private Hash hash;
-
-    // Constructor for mining the block
+    
     public Block(int num, int amount, Hash prevHash) {
         this.num = num;
         this.amount = amount;
+        this.nonce = mineBlock();
         this.prevHash = prevHash;
-        mineBlock();
+        this.hash = calculateHash();
+        // Implement the mining operation to discover the nonce and compute the hash
+        // Use the MessageDigest class and ByteBuffer for this
     }
+   
 
     // Constructor with provided nonce (no need to mine)
     public Block(int num, int amount, Hash prevHash, long nonce) {
@@ -58,8 +62,19 @@ public class Block {
                 amount, nonce, prevHash.toString(), hash.toString());
     }
 
+public static byte[] calculateHash(String msg) throws NoSuchAlgorithmException {
+    MessageDigest md = MessageDigest.getInstance("sha-256");
+    md.update(msg.getBytes());
+    byte[] hash = md.digest();
+    return hash;
+    } // calculateHash(String)
+
     // Helper method for mining a block (you can call this method from the mining constructor)
-    private void mineBlock() {
+    private long mineBlock() {
+        long nonce;
+    
+      nonce = rand.nextLong();
+      hash = computeHash(num, amount, prevHash, nonce);
         // Implement the mining logic here
         // Use MessageDigest and ByteBuffer to update the hash with relevant block information
         // Search the space of nonces by iterating over all possible long values
